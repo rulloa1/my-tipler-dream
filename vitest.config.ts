233f2ad09ac4ac547ea25e87792a3ef954c41482
Rory Roy Ulloa <rulloa1@my.devry.edu>
2025-12-26 17:14:27 -0600

    fix: Add type assertion for the React plugin in Vitest configuration.

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-    plugins: [react()],
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    plugins: [react() as any],
     test: {
         environment: 'jsdom',
         globals: true,
